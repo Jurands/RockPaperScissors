1d8605c4f2414e1e82c53e17b50ad7839abec815
Jurands <daarkchannel@gmail.com>
2022-10-23 23:17:41 -0300

    Added a result() function to keep track of number of wins the player and the computer have, also reworked a bit of the previous functions

--- scripts.js
+++ scripts.js
@@ -1,43 +1,71 @@
 const choices = ['rock', 'paper', 'scissors']
-let playerSelection
-let computerSelection
+let playerSelection = getPlayerChoice()
+let computerSelection = getComputerChoice()
+let result
+
+function getPlayerChoice() {
+    let playerChoice = prompt()
+    if (playerChoice === 'rock'){
+        return(playerChoice)
+    }
+    if (playerChoice === 'paper'){
+        return playerChoice
+    }
+    if (playerChoice === 'scissors'){
+        return playerChoice
+    }
+    }
 
 function getComputerChoice() {
     //generate a random number using the index of the choices array and return a string
     let randomNumber = choices[Math.floor(Math.random() *choices.length)]
-
     if(randomNumber === 1) {
-        computerSelection = 'rock'
+        return 'rock'
     }
     if(randomNumber === 2) {
-        computerSelection = 'paper'
+        return 'paper'
     }
     if(randomNumber === 3) {
-        computerSelection = 'scissors'
+        return 'scissors'
     }
+    return randomNumber
 }
 
 function playRound(playerSelection, computerSelection) {
     //plays a single round of Rock Paper and Scissors and return a string with the result
+    let playerWins = "You Win!"
+    let computerWins = "Computer Wins!"
+    let draw = "Its a Draw!"
     if(computerSelection === playerSelection) {
-        result = "Its a Draw!"
+        return draw
     }
     if(computerSelection === 'rock' && playerSelection === 'paper') {
-        result = "you WIN!"
+        return playerWins
     }
     if(computerSelection === 'rock' && playerSelection === 'scissors') {
-        result = "you LOST!"
+        return computerWins
     }
     if(computerSelection === 'paper' && playerSelection === 'scissors') {
-        result = "you WIN!"
+        return playerWins
     }
     if(computerSelection === 'paper' && playerSelection === 'rock') {
-        result = "you LOST!"
+        return computerWins
     }
     if(computerSelection === 'scissors' && playerSelection === 'rock') {
-        result = "you WIN!"
+        return playerWins
     }
     if(computerSelection === 'scissors' && playerSelection === 'paper') {
-        result = "you LOST!"
+        return computerWins
+    }
+    if (playerWins) {
+        return player
+    } else if (draw) {
+        return draw
+    } else {
+        computerWins
     }
 }
+
+function game() {
+
+    }
